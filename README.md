26cea31c1801b2329fa7e5b4170904bba2b7db84
radu-bordea <radu.bordea.dev@gmail.com>
2025-02-16 19:15:33 +0100

    create server, product model and database connection

---
+++ README.md
@@ -0,0 +1,7 @@
+// commands
+- npm init -y // run on the root level
+- npm install express mongoose dotenv
+- nodemon i -D
+
+// Add "type": "module", to packet.json
+
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); // allow us to accept json data in the req body
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fieldds" });
+  }
+
+  const newProduct = await new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (err) {
+    console.error("Error creating the product", err.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});
